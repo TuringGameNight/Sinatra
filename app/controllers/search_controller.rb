5a292a2031dad5a25051427a0f824ddfcf42b7f1
Eduardo Parra <helloeduardo@icloud.com>
2021-01-07 10:01:41 -0700

    Remove name wildcard in route
    Co-Authored-By: Austin Aspaas <araspaas@live.com>
    Co-Authored-By: Garrett Cottrell <GarrettCottrell@users.noreply.github.com>
    Co-Authored-By: Grant Dempsey <GDemps@users.noreply.github.com>
    Co-Authored-By: Greg Mitchell <GregJMitchell@users.noreply.github.com>
    Co-Authored-By: Kate Tester <katemorris@gmail.com>
    Co-Authored-By: Shaunda Cunningham <smcunning@users.noreply.github.com>
    Co-Authored-By: null <ShaunDaneJames@users.noreply.github.com>
    Co-Authored-By: Philip DeFraties <PhilipDeFraties@users.noreply.github.com>
    Co-Authored-By: Taylor Phillips <taylorscottphillips@gmail.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ require './config/environment'
 
 class SearchController < ApplicationController
 
-  get '/api/v1/search/:name' do
+  get '/api/v1/search' do
     conn = Faraday.new(
       url: 'https://api.boardgameatlas.com/api/',
       params: { client_id: ENV['CLIENT_ID'] }
