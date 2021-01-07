30bacdf76c1f7828981099a7e9906aa225bba66e
Eduardo Parra <helloeduardo@icloud.com>
2021-01-06 19:29:30 -0700

    Add Figaro Loading of Environment Variables
    Co-Authored-By: Grant Dempsey <GDemps@users.noreply.github.com>

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,9 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require './app/controllers/application_controller'
 require_all 'app'
+
+Figaro.application = Figaro::Application.new(
+  environment: 'production',
+  path: File.expand_path("config/application.yml")
+)
+Figaro.load
