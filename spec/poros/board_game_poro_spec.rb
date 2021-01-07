8a0b6109ad2a58a8d08504ada0f0a9fcab645a2d
Eduardo Parra <helloeduardo@icloud.com>
2021-01-07 16:37:19 -0700

    Change duration expectation
    Co-Authored-By: Taylor Phillips <taylorscottphillips@gmail.com>

--- spec/poros/board_game_poro_spec.rb
+++ spec/poros/board_game_poro_spec.rb
@@ -6,13 +6,13 @@ end
 
 RSpec.describe 'BoardGame' do
   it 'exists and has attributes' do
-    board_games = BoardGamesService.find_board_games('catan') 
+    board_games = BoardGamesService.find_board_games('catan')
     game = BoardGame.new(board_games.first)
 
     expect(game.name).to eq('Catan')
     expect(game.description).to eq(board_games.first[:description])
     expect(game.age_range).to eq('10+')
-    expect(game.duration).to eq(45)
+    expect(game.duration).to eq(90)
     expect(game.image).to eq('https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559258096678-51Eiofu9mqL.jpg')
     expect(game.type).to eq(nil)
   end
