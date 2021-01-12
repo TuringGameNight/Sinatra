17fa5359947dc97fa0c79ced6a08421ef32a08e6
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-11 21:18:21 -0700

    Adds method to translate game id to game type

--- app/poros/board_game.rb
+++ app/poros/board_game.rb
@@ -3,7 +3,7 @@
 class BoardGame
   attr_reader :name, :description, :duration, :image, :game_type
 
-  def initialize(data, categories = nil)
+  def initialize(data, categories)
     @name = data[:name]
     @description = data[:description]
     @min_age = data[:min_age]
@@ -11,7 +11,9 @@ class BoardGame
     @image = data[:image_url]
     @min_players = data[:min_players]
     @max_players = data[:max_players]
-    @game_type = categories
+    @available_categories = categories
+    @game_type_ids = data[:categories]
+    @game_type = []
   end
 
   def age_range
@@ -28,5 +30,18 @@ class BoardGame
     else
       "#{@min_players}-#{@max_players}"
     end
+  end
+
+  def game_type
+    @game_type_ids.each do |game_category|
+      @available_categories.each do |category|
+        if game_category[:id] == category.id
+          @game_type << category.name
+        end
+      end
+    end
+    return nil if @game_type.empty?
+
+    @game_type.join(', ')
   end
 end
