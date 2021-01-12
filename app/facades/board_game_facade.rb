5b057dcd52a501f062112d6e0403762c3fa78f0f
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-12 08:27:53 -0700

    Refactors category attribution to be faster

--- app/facades/board_game_facade.rb
+++ app/facades/board_game_facade.rb
@@ -5,9 +5,24 @@ class BoardGamesFacade
     data = BoardGamesService.find_board_games(name)
 
     data.map do |game|
-      categories = BoardGamesFacade.category
+      categories = BoardGamesFacade.translate_ids(game)
       BoardGame.new(game, categories)
     end
+  end
+
+  def self.translate_ids(game)
+    translated_categories = []
+    categories = BoardGamesFacade.category
+
+    game[:categories].each do |game_category|
+      categories.each do |category|
+        if game_category[:id] == category.id
+          translated_categories << category.name
+        end
+      end
+    end
+    return nil if translated_categories.empty?
+    translated_categories.join(', ')
   end
 
   def self.category
